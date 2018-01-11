ad75d79b0aaa65961196c851c68383441ebecab7
zerox029 <zerox029@gmail.com>
2018-01-10 23:13:04 -0500

    Added an embed to the osu profile command

--- src/Commands/Games/Osu.js
+++ src/Commands/Games/Osu.js
@@ -1,5 +1,6 @@
 const Commando = require("discord.js-commando"),
-      rp = require('request-promise');
+      Discord  = require("discord.js"),
+      rp       = require('request-promise');
 
 module.exports = class ProfileCommand extends Commando.Command
 {
@@ -18,15 +19,13 @@ module.exports = class ProfileCommand extends Commando.Command
                 },
                 {
                     key: 'specifier',
-                    prompt: 'Depends on what info you are looking for?\n',
+                    prompt: 'Who do you wish to stalk?\n',
                     type: 'string'
                 },
                 {
                     key: 'gamemode',
-                    prompt: 'What gamemode are you looking for?\n `0: Standard | 1: Taiko | 2: CtB | 3: Mania`\n',
-                    type: 'integer',
-                    min: 0,
-                    max: 3
+                    prompt: 'What gamemode are you looking for?\n `Standard | Taiko | CtB | Mania`\n',
+                    type: 'string',
                 }
             ]
         });
@@ -39,42 +38,57 @@ module.exports = class ProfileCommand extends Commando.Command
         switch(args.type)
         {
             case "profile":
-                this.args[1].prompt = "Who do you wish to stalk?\n";
+                var gamemodeInt = this.getGamemodeInteger(args.gamemode)
 
-                var url = "https://osu.ppy.sh/api/get_user?k=" + process.env.OSU_API_KEY + "&u=" + args.specifier + "&m=" + args.gamemode;
-                
-                var profileData = await rp(url, function(error, response, body) 
+                if(gamemodeInt != null)
                 {
-                    return body;
-                });
+                    var url = "https://osu.ppy.sh/api/get_user?k=" + process.env.OSU_API_KEY + "&u=" + args.specifier + "&m=" + gamemodeInt;
 
-                var profileReply = await this.createProfileReply(profileData);
-                message.channel.send(profileReply);
+                    var profileData = await rp(url, function(error, response, body) 
+                    {
+                        return body;
+                    });
+    
+                    var profileReply = await this.createProfileReply(profileData, message);
+                    message.channel.send({embed: profileReply});
+                }
+                else
+                {
+                    message.reply("Invalid  gamemode! >_<\n I support `standard, mania, taiko and catch the beat`");
+                }
 
                 break;
 
-            // case "top scores":
-            //     this.args[1].prompt = "Who do you wish to stalk?\n";
-
-            //     var url = "https://osu.ppy.sh/api/get_user_best?k=" + process.env.OSU_API_KEY + "&u=" + args.specifier + "&m=" + args.gamemode + "&limit=10";
-
-            //     var scoresData = await rp(url, function(error, response, body) 
-            //     {
-            //         return body;
-            //     });
-
-            //     var scoresReply = await this.createScoresReply(profileData);
-            //     message.channel.send(scoresReply);
-
-            //     break;
-
             default:
-                message.reply("Invalid query type! >_<\n I only support `profile`")
+                message.reply("Invalid query type! >_<\n I only support `profile`");
                 break;
         }
     }
 
-    createProfileReply(profileData)
+    //Returns the integer corresponing to the specified gamemode
+    //Returns null if the specified gamemode is invalid
+    getGamemodeInteger(gamemode)
+    {
+        switch(gamemode.toLowerCase())
+        {
+            case "standard":
+                return 0;
+            case "std":
+                return 0;
+            case "taiko":
+                return 1;
+            case "ctb":
+                return 2;
+            case "catch the beat":
+                return 2;
+            case "mania":
+                return 3;
+            default:
+                return null;
+        }
+    }
+
+    createProfileReply(profileData, message)
     {
         var profileData = JSON.parse(profileData);
         profileData = profileData[0];
@@ -83,8 +97,44 @@ module.exports = class ProfileCommand extends Commando.Command
         reply += "Score:   " + profileData.ranked_score + "(#" + profileData.pp_rank + ")\n";
         reply += "Plays:   " + profileData.playcount + "(lvl" + profileData.level + ")\n";
         reply += "Accuracy:   " + profileData.accuracy + "%"; 
+        
+        const profileUrl = "http://osu.ppy.sh/u/" + profileData.user_id;
+        const thumbnailUrl = "https://a.ppy.sh/" + profileData.user_id;
 
-        return reply;
+        const roundedUserLevel = Math.floor(profileData.level);
+        const roundedPP = Math.round(profileData.pp_raw);
+        const roundedAcc = Math.round(profileData.accuracy * 100) / 100;
+
+        const rankedScoreText = "Score:    " + profileData.ranked_score;
+        const lvlText = " (lvl" + roundedUserLevel + ")";
+        const ppText = "Performance Points:    " + roundedPP;
+        const rankText = " (#" + profileData.pp_rank + ")";
+        const accText = "Accuracy:    " + roundedAcc + "%";
+
+        const completeScoreText = rankedScoreText + lvlText + "\n" + ppText + rankText + "\n" + accText;
+
+        const embed = {
+            "color": 1566961,
+            "footer": {
+              "text": message.author.username + " ran this command"
+            },
+            "thumbnail": {
+              "url": thumbnailUrl
+            },
+            "author": {
+              "name": "Osu",
+              "url": profileUrl,
+              "icon_url": "https://orig00.deviantart.net/bff0/f/2015/202/8/7/minimalistic_patterned___osu__logo_remake_by_glitchypsix-d92ap0c.png"
+            },
+            "fields": [
+              {
+                "name": "Stats for " + profileData.username + ": " + profileUrl,
+                "value": completeScoreText
+              }
+            ]
+          };
+
+        return embed;
     }
 
     createScoresReply(scoresData)
