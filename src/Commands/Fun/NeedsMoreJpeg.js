b1218b07d0a0d37d0dcf2a3388e9dae7fed0bb25
zerox029 <zerox029@gmail.com>
2018-01-13 13:39:48 -0500

    Fixed NeedsMoreJpeg

--- src/Commands/Fun/NeedsMoreJpeg.js
+++ src/Commands/Fun/NeedsMoreJpeg.js
@@ -1,4 +1,7 @@
-const Commando = require("discord.js-commando");
+const Commando = require("discord.js-commando"),
+      Jimp = require("jimp"),
+      path = require("path"),
+      isImageUrl = require("is-image-url");
 
 module.exports = class NeedsMoreJpeg extends Commando.Command
 {
@@ -9,13 +12,83 @@ module.exports = class NeedsMoreJpeg extends Commando.Command
             aliases: ['jpg', 'needsmorejpeg', 'needsmorejpeg', 'jpgify', 'jpegify', 'j'],
             group: 'fun',
             memberName: "jpegify",
-            description: "The solution to all your jpeg making problems... - **DOESN'T WORK**"
+            description: "The solution to all your jpeg making problems...",
+            args: [
+                {
+                    key: "link",
+                    prompt: "A link to an image, optional",
+                    type: "string",
+                    default: ''
+                }
+            ]
         });
     }
 
     async run(message, args)
     {
-        var Attachments = (message.attachments).array();
-        message.reply('http://morejpeg.com/Image/JPEG/b14252cf-9cf4-e711-80e6-bb81c215c2dd');
+        try
+        {
+            var attachments = (message.attachments).array();
+            var imgLink;
+
+            if(attachments === undefined || attachments.length == 0)
+            {
+                if(args.link == '')
+                {
+                    throw "You haven't given me any image to jpegify";
+                }
+                else
+                {
+                    imgLink = args.link;
+                }
+            }
+            else
+            {
+                var imgLink = attachments[0].url;
+            }
+            
+            if(!isImageUrl(imgLink))
+            {
+                throw "The file you sent me is invalid";
+            }
+
+            var newFileName = this.makeID();
+            var masterFolderPath = path.join(__dirname, '..', '..', '..', 'Data', 'MoreJpeg');
+            var newImagePath = masterFolderPath + "/"+ newFileName + ".jpeg"
+
+            await Jimp.read(imgLink).then(function (img) 
+            {
+                img.quality(2)
+                   .write(newImagePath);
+            })
+            .catch(function (err) 
+            {
+                if(err.code == 'ENOENT')
+                    err = "You didn't provide me with a valid image";
+
+                message.reply(err);
+            });
+
+            message.say("I am done!", {
+                file: newImagePath
+            });
+        }
+        catch(err)
+        {
+            message.reply(err);
+        }
+    }
+
+    makeID()
+    {
+        var text = "";
+        var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+        for(var i = 0; i < 8; i++)
+        {
+            text += possible.charAt(Math.floor(Math.random() * possible.length));
+        }
+
+        return text;
     }
 }
