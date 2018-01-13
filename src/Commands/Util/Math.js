d77185f60e227c623a3ee6942efafa82f24e994c
zerox029 <zerox029@gmail.com>
2018-01-12 19:10:20 -0500

    Added math command

---
+++ src/Commands/Util/Math.js
@@ -0,0 +1,36 @@
+const Commando = require("discord.js-commando"),
+      math = require("mathjs");
+
+module.exports = class DiceRollCommand extends Commando.Command
+{
+    constructor(client)
+    {
+        super(client, {
+            name: "math",
+            group: 'util',
+            memberName: "math",
+            description: "Evaluates a mathematical expression",
+            args: [
+                {
+                    key: "expression",
+                    prompt: "What do you want me to calculate?\n",
+                    type: "string"
+                }
+            ]
+        });
+    }
+
+    async run(message, args)
+    {
+        try
+        {
+            var answer = math.eval(args.expression);
+            message.say("I evaluate ` " + args.expression + " ` to equal ` " + answer + " `");
+        }
+        catch(err)
+        {
+            message.reply("Sorry! I could not evaluate the given expression");
+            console.log(err);
+        }
+    }
+}
